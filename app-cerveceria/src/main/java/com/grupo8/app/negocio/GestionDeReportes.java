d043107587f1e716997e9dca4016129c1b57373b
facuerrobidart <85039144+facuerrobidart@users.noreply.github.com>
2022-11-07 20:40:32 -0300

    reportes

--- app-cerveceria/src/main/java/com/grupo8/app/negocio/GestionDeReportes.java
+++ app-cerveceria/src/main/java/com/grupo8/app/negocio/GestionDeReportes.java
@@ -1,10 +1,15 @@
 package com.grupo8.app.negocio;
 
+import com.grupo8.app.dto.MozoDTO;
 import com.grupo8.app.dto.ReporteMesaDto;
+import com.grupo8.app.dto.ReporteVentaDto;
+import com.grupo8.app.excepciones.EntidadNoEncontradaException;
+import com.grupo8.app.excepciones.MalaSolicitudException;
 import com.grupo8.app.modelo.CierreComanda;
 import com.grupo8.app.modelo.Empresa;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,6 +43,58 @@ public class GestionDeReportes {
     }
 
     return resultado;
+  }
+
+  private List<ReporteVentaDto> generarReporteDeVentas() {
+    List<CierreComanda> cierres = empresa.getCierreComandas();
+    List<ReporteVentaDto> resultado = new ArrayList<>();
+
+    for (CierreComanda cierre : cierres) {
+      MozoDTO mozo = MozoDTO.of(cierre.getMesa().getMozoAsignado());
+      Optional<ReporteVentaDto> yaExiste = resultado.stream().filter(r -> r.getMozo().getId().equals(mozo.getId())).findFirst();
+
+      if (yaExiste.isPresent()) { //si existe el mozo en el reporte lo actualizo
+        ReporteVentaDto reporte = yaExiste.get();
+        reporte.setTotal(reporte.getTotal() + cierre.getTotal());
+      } else { //sino lo agrego
+        ReporteVentaDto reporte = new ReporteVentaDto();
+        reporte.setMozo(mozo);
+        reporte.setTotal(cierre.getTotal());
+        resultado.add(reporte);
+      }
+    }
+
+    return resultado;
+  }
+
+  public ReporteVentaDto maximo() throws MalaSolicitudException {
+    Optional<ReporteVentaDto> maximo = generarReporteDeVentas().stream().max(Comparator.comparing(ReporteVentaDto::getTotal));
+
+    if (maximo.isPresent()) {
+      return maximo.get();
+    } else {
+      throw new MalaSolicitudException("No hay reportes de ventas");
+    }
+  }
+
+  public ReporteVentaDto minimo() throws MalaSolicitudException {
+    Optional<ReporteVentaDto> minimo = generarReporteDeVentas().stream().min(Comparator.comparing(ReporteVentaDto::getTotal));
+
+    if (minimo.isPresent()) {
+      return minimo.get();
+    } else {
+      throw new MalaSolicitudException("No hay reportes de ventas");
+    }
+  }
+
+  public ReporteVentaDto reporteDeEmpleado(MozoDTO mozo) throws EntidadNoEncontradaException {
+    Optional<ReporteVentaDto> reporte = generarReporteDeVentas().stream().filter(r -> r.getMozo().getId().equals(mozo.getId())).findFirst();
+
+    if (reporte.isPresent()) {
+      return reporte.get();
+    } else {
+      throw new EntidadNoEncontradaException("No se encontró el reporte del mozo");
+    }
   }
 
 
